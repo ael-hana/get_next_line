09bef86c3d749a0c264be7ec8106c5e7ac8b726b
Anasse EL-HANANI <ael-hana@e1r1p3.42.fr>
2015-12-18 14:53:53 +0100

    Je veux pas ton facebook TFOU

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,17 @@
 /*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 16:06:18 by ael-hana          #+#    #+#             */
-/*   Updated: 2015/12/18 14:25:15 by ael-hana         ###   ########.fr       */
+/*   Updated: 2015/12/18 14:49:50 by ael-hana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-list_fd					*ft_init_list(int fd, list_fd *next, int i)
+t_list_fd					*ft_init_list(int fd, t_list_fd *next, int i)
 {
-	list_fd				*list;
+	t_list_fd				*list;
 
-	if (!(list = (list_fd *)malloc(sizeof(list_fd))))
+	if (!(list = (t_list_fd *)malloc(sizeof(t_list_fd))))
 		return (NULL);
 	list->fd = fd;
 	list->next = next;
@@ -25,10 +25,10 @@ list_fd					*ft_init_list(int fd, list_fd *next, int i)
 	return (list);
 }
 
-list_fd					*ft_search_fd(list_fd *list, int fd)
+t_list_fd					*ft_search_fd(t_list_fd *list, int fd)
 {
-	void				*tmp;
-	list_fd				*ltmp;
+	void					*tmp;
+	t_list_fd				*ltmp;
 
 	if (!list)
 		return (ft_init_list(fd, NULL, -1));
@@ -47,18 +47,19 @@ list_fd					*ft_search_fd(list_fd *list, int fd)
 		ltmp = list;
 		list = list->next;
 	}
-	return (ltmp->next = ft_init_list(fd, tmp,ltmp->i));
+	return (ltmp->next = ft_init_list(fd, tmp, ltmp->i));
 }
 
-char					check_buf(list_fd *list, char **line, char *buf)
+char						check_buf(t_list_fd *list, char **line, char *buf)
 {
-	char	*ptr;
-	void	*tmp;
+	char					*ptr;
+	void					*tmp;
 
 	if ((ptr = ft_strchr(buf, '\n')))
 	{
 		*ptr = '\0';
-		*line = ft_strjoin(tmp = *line, buf);
+		tmp = *line;
+		*line = ft_strjoin(*line, buf);
 		free(list->content);
 		list->content = ft_strdup(++ptr);
 		free(tmp);
@@ -68,7 +69,8 @@ char					check_buf(list_fd *list, char **line, char *buf)
 	else if ((ptr = ft_strchr(list->content, '\n')))
 	{
 		*ptr = '\0';
-		*line = ft_strdup(tmp = list->content);
+		tmp = list->content;
+		*line = ft_strdup(list->content);
 		list->content = ft_strdup(++ptr);
 		free(tmp);
 		free(buf);
@@ -77,12 +79,12 @@ char					check_buf(list_fd *list, char **line, char *buf)
 	return (0);
 }
 
-int						get_next_line(int const fd, char **line)
+int							get_next_line(int const fd, char **line)
 {
-	static list_fd		*list = NULL;
-	char				*buf;
-	int					len;
-	char				*chr;
+	static t_list_fd		*list = NULL;
+	char					*buf;
+	int						len;
+	char					*chr;
 
 	if (fd < 0 || !line || !(list = ft_search_fd(list, fd)) || BUFF_SIZE <= 0)
 		return (-1);
@@ -99,7 +101,8 @@ int						get_next_line(int const fd, char **line)
 		buf[len] = '\0';
 		if (check_buf(list, line, buf))
 			return (1);
-		*line = ft_strjoin((chr = *line), buf);
+		chr = *line;
+		*line = ft_strjoin(*line, buf);
 		free(chr);
 	}
 	return (0);
